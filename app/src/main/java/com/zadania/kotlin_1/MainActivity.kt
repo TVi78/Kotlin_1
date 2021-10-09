dbdbfe7ec509d1dd4043c7731c538858a2a93f45
Виктория Тарасова <viky.ne2@gmail.com>
2021-10-09 15:59:00 +0300

    Первый урок. 1. Добавлена кнопка в разметку

--- app/src/main/java/com/zadania/kotlin_1/MainActivity.kt
+++ app/src/main/java/com/zadania/kotlin_1/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-
+public var n=0
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         val btn=findViewById<Button>(R.id.btn1)
         btn.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
-                btn.setText("Нажато")
-                    //      TODO("Not yet implemented")
+                btn.setText(n.toString())
+                n+=1
             }
         })
     }
