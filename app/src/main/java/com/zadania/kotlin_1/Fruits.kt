727988b86494eb6b235ce94c5c480ad0f74c8192
Виктория Тарасова <viky.ne2@gmail.com>
2021-10-16 00:41:09 +0300

    2. а) Сформирован data class с двумя свойствами б) В Object вызвано copy и выведено значение скопированного класса на экран

---
+++ app/src/main/java/com/zadania/kotlin_1/Fruits.kt
@@ -0,0 +1,10 @@
+package com.zadania.kotlin_1
+
+object Fruits {
+
+    val newFruit = Fruit1().copy()
+    fun getFruit(): String {
+        return newFruit.name
+    }
+}
+data class Fruit1(val name: String="Груша", val number: Int=15)
--- app/src/main/java/com/zadania/kotlin_1/MainActivity.kt
+++ app/src/main/java/com/zadania/kotlin_1/MainActivity.kt
@@ -4,19 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-public var n=0
+import android.widget.TextView
+
+public var n = 0
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn=findViewById<Button>(R.id.btn1)
-        btn.setOnClickListener(object : View.OnClickListener{
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val text1 = findViewById<TextView>(R.id.text1)
+        val text2 = findViewById<TextView>(R.id.text2)
+        btn1.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                btn.setText(n.toString())
-                n+=1
+                btn1.setText(n.toString())
+                n += 1
             }
         })
+        btn2.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                n=0
+                btn1.setText("Нажмите")
+            }
+        })
+        val Fruit0 = Fruit("Яблоко", 10)
+        text1.setText("Фрукт: " + Fruit0.name.toString() + ", кол-во: " + Fruit0.number.toString())
+        val fruitData = Fruits.getFruit()
+        text2.setText(fruitData.toString())
     }
+
+    data class Fruit(val name: String, val number: Int)
+
 }
 
 
